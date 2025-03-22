753fe053340c8b463037fe7ebf3400fcddb5f113
Rususlan <ruslankapustian001@gmail.com>
2025-03-22 17:43:09 +0200

    11

---
+++ 11/ES6Synt.js
@@ -0,0 +1,31 @@
+let fArr = [1,2,3,4];
+console.log(fArr);
+let sArr = [...fArr,5,6,7,8];
+console.log(sArr);
+
+function Ffunction(Str,...arg){
+    console.log(Str,arg)
+}
+Ffunction("Hello",1,2,3,4)
+
+function getAverage(...numbers) {
+    if (numbers.length === 0) return 0; 
+
+    let sum = 0;
+    for (let num of numbers) {
+        sum += num;
+    }
+
+    return sum / numbers.length;
+}
+console.log(getAverage(5, 15));
+
+function TOF(args) {
+    console.log(`Tipe of this information is ${typeof args}`);
+}
+TOF(true);
+let sumet = prompt("Enter summething like Bolean, number or string:");
+function TOFT(args) {
+    alert(`Tipe of this information is ${typeof args}`);
+}
+TOFT(sumet);
