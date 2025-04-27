9d2f12dbb814a42ecd664d552e8f61e308b037d4
Rususlan <ruslankapustian001@gmail.com>
2025-04-28 00:09:10 +0300

    19

---
+++ 19/RegEx.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    let textStr = "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos itaque vero laborum fugit harum! Quia, cumque necessitatibus nemo veniam odit dolor impedit aspernatur, ipsa vitae minima quasi doloribus repellendus cupiditate!";
+
+    let matches = textStr.match(/[A-Z]/g);
+
+    console.log(matches);
+
+    let operation = "5 плюс 7 = 3";
+    let operationMatches = operation.match(/\d/g);
+    console.log(operationMatches);
+
+    let matches2 = textStr.match(/\w\w\w\w\w/g);
+    console.log(matches2);
+}
